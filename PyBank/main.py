313460202e2a31b6ccd49bc65c5eb0320d6f8b3c
Katie Djahan <katiedjahan@yahoo.com>
2023-03-17 10:10:53 -0700

    added results text file

---
+++ PyBank/main.py
@@ -0,0 +1,13 @@
+# PyBank Challenge:
+
+# Create a Python script that analyzes the records (budget_data.csv) to calculate
+# each of the following values:
+    # The total number of months included in the dataset
+    # The net total amount of "Profit/Losses" over the entire period
+    # The changes in "Profit/Losses" over the entire period, and then the 
+    # average of those changes
+    # The greatest increase in profits (date and amount) over the entire period
+    # The greatest decrease in profits (date and amount) over the entire period
+# Your final script should both print the analysis to the terminal and export a 
+# text file with the results
+
---
+++ PyPoll/main.py
@@ -0,0 +1,12 @@
+# PyPoll Challenge
+
+# Create a Python script that analyzes the votes (election_data.csv) and calculates
+# each of the following values:
+    # The total number of votes cast
+    # A complete list of candidates who received votes
+    # The percentage of votes each canddiate won
+    # The total number of votes each candidate won
+    # The winner of the eelction based on popular vote
+# Your final script should both print the analysis to the terminal and export a 
+# text file with the results
+
