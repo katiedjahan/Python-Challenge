e328f75148ba0dce1f77166f711faa0ff18f8c22
Katie Djahan <katiedjahan@yahoo.com>
2023-03-18 17:20:09 -0700

    PyBank work progress

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,29 +10,57 @@
 # Your final script should both print the analysis to the terminal and export a 
 # text file with the results
 
+
+# import modules
 import csv
 import os
 
 
-INPUT_PATH = os.path.join("Resources", "budget_data.csv")
-OUTPUT_PATH = os.path.join("Analysis", "results.txt")
-
+# define what is being read and where to write results
+input_path = os.path.join("Resources", "budget_data.csv")
+output_path = os.path.join("Analysis", "results.txt")
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
-print(os.getcwd())
 
-# csv_file_object = open(INPUT_PATH)
-# print(csv_file_object.read())
-# csv_file_object.close()
+# create lists
+months = []
+total = []
+pl = []
+pl_change = []
 
-with open(INPUT_PATH) as csv_file_object:
-    print(csv_file_object.read())
 
-results = (
-    "TBD\n"
-    "Done\n"
-)
+# read
+with open(input_path, 'r') as csv_file_object:
+    csvreader = csv.reader(csv_file_object, delimiter=",")
+    next(csvreader) # skip header
+    for row in csvreader:
+        months.append(row[0])
+        total.append(int(row[1]))
+        pl.append(row[1])
+for i in range(1, len(pl)):
+    change = int(pl[i]) - int(pl[i - 1])
+    pl_change.append(change)
 
-with open(OUTPUT_PATH, 'w') as output_file_object:
-    print(results)
-    output_file_object.write(results)
+avg_change = int(sum(pl_change))/int(len(pl_change))
+avg_change = "$" + str(round(avg_change, 2))
+
+
+    
+ 
+
+# write
+with open(output_path, 'w') as file_writer:
+    file_writer.write("Financial Analysis\n")
+    file_writer.write("--------------------\n")
+    file_writer.write(f"Total months: {len(months)}\n")
+    file_writer.write(f"Total: ${sum(total)}\n")
+    file_writer.write(f"Average Change: {avg_change}\n")
+    file_writer.write(f"Greatest Increase in Profits: \n")
+    file_writer.write(f"Greatest Decrease in Profits: \n")
+    print("Financial Analysis\n")
+    print("--------------------\n")
+    print(f"Total months: {len(months)}\n")
+    print(f"Total: ${sum(total)}\n")
+    print(f"Average Change: {avg_change}\n")
+    print(f"Greatest Increase in Profits: \n")
+    print(f"Greatest Decrease in Profits: \n")
